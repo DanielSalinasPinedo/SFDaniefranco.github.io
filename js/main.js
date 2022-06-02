1bd3c1c53532454fc7398529315838d882c85485
SFDaniefranco <SFDaniefranco@gmail.com>
2022-06-01 20:03:02 -0500

    segunda parte

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { data } from "./preguntas.js";
+
 const contenedor = document.querySelector(".contenedor");
 const contEncabezado = document.createElement("div");
 const contPregunta = document.createElement("div");
@@ -7,9 +9,12 @@ const contCatParrafo = document.createElement("p")
 const contBtn = document.createElement("div")
 
 contEncabezado.className = "encabezado";
-contPregunta.className = "pregunta";
-contCategoria.className = "categoria";
 
+contPregunta.className = "pregunta";
+contPregunta.id = "pregunta"
+
+contCategoria.className = "categoria";
+contCategoria.id = "categoria";
 
 contPreParrafo.textContent = "Pregunta";
 contCatParrafo.textContent = "Categoria";
@@ -28,6 +33,38 @@ for(var i = 1; i<=4; i++){
     contEncabezado.append(contBtn)
 }
 
-
 contenedor.appendChild(contEncabezado)
 
+var myArray = ['1','2','3','4'];
+var i,j,k;
+for (i = myArray.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    k = myArray[i - 1];
+    myArray[i - 1] = myArray[j];
+   myArray[j] = k;
+}
+console.log(myArray)
+
+llenarPreguntas();
+
+function llenarPreguntas(){
+    contCatParrafo.textContent = data[0].categoria;
+    contPreParrafo.textContent = data[0].pregunta;
+    getElement("btn"+myArray[0]).textContent = data[0].respuesta;
+    getElement("btn"+myArray[1]).textContent = data[0].incorrecta1;
+    getElement("btn"+myArray[2]).textContent = data[0].incorrecta2;
+    getElement("btn"+myArray[3]).textContent = data[0].incorrecta3;
+}
+
+/*data.forEach(pregunta => {
+    contCatParrafo.textContent = pregunta.categoria;
+    contPreParrafo.textContent = pregunta.pregunta;
+    getElement("btn"+myArray[0]).textContent = pregunta.respuesta;
+    getElement("btn"+myArray[1]).textContent = pregunta.incorrecta1;
+    getElement("btn"+myArray[2]).textContent = pregunta.incorrecta2;
+    getElement("btn"+myArray[3]).textContent = pregunta.incorrecta3;
+});*/
+
+function getElement(id){
+    return document.getElementById(id);
+}
---
+++ js/preguntas.js
@@ -0,0 +1,18 @@
+export const data =[
+    {
+        "categoria" : "Arte y literatura",
+        "pregunta" : "¿Quien pinto la mona lisa?",
+        "respuesta" : "Leonardo da Vinci",
+        "incorrecta1" : "Picasso",
+        "incorrecta2" : "Miguel Angel",
+        "incorrecta3" : "Monet"
+    },
+    {
+        "categoria" : "Arte y literaturaa",
+        "pregunta" : "¿Quien pinto la mona lisa?a",
+        "respuesta" : "Leonardo da Vincia",
+        "incorrecta1" : "Picassoa",
+        "incorrecta2" : "Miguel Angela",
+        "incorrecta3" : "Moneta"
+    }
+];
